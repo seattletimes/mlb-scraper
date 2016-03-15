337b3f7a192b85a8c5c346b3e2b17a38d20bec9b
Thomas Wilburn <news@thomaswilburn.net>
2016-03-15 09:19:00 -0700

    Improved test display

--- tests.js
+++ tests.js
@@ -3,6 +3,7 @@ var cheerio = require("cheerio");
 var request = require("request");
 var gameday = require("./gameday");
 var pg = require("pg");
+var chalk = require("chalk");
 
 var database = new pg.Client(require("./creds.json"));
 database.connect();
@@ -15,8 +16,8 @@ var results = {
 };
 
 //randomly test five games
-for (var i = 0; i < 5; i++) {
-  var year = Math.round(Math.random() * 5) + 2009;
+for (var i = 0; i < 20; i++) {
+  var year = Math.round(Math.random() * 5) + 2008;
   var month = Math.round(Math.random() * 4) + 5;
   var day = Math.round(Math.random() * 27) + 1;
   tests.push({
@@ -25,7 +26,11 @@ for (var i = 0; i < 5; i++) {
       gameday.getGames(year, month, day, function(err, games) {
         if (err) return callback(err);
         var game = games[Math.floor(Math.random() * games.length)];
-        console.log(`Game is ${game.home} vs ${game.away}`);
+        if (game.home == 'tba' || game.away == 'tba') {
+          console.log("  Game is TBA, skipping")
+          return callback();
+        }
+        console.log(`  Game is ${game.home.toUpperCase()} vs ${game.away.toUpperCase()}`);
         var url = gameday.makeGameURL(game) + "/inning/inning_all.xml";
         request(url, function(err, response, body) {
           if (err) return callback(err);
@@ -36,29 +41,28 @@ for (var i = 0; i < 5; i++) {
               pfx_x: parseFloat(p.attribs.pfx_x),
               end_speed: parseFloat(p.attribs.end_speed)
             };
-          });
-          console.log(`Checking ${pitches.length} pitches`);
+          }).filter(p => p.id);
+          console.log(`    Checking ${pitches.length} pitches`);
           async.eachSeries(pitches, function(pitch, c) {
-            database.query(`SELECT * FROM pitches WHERE id = '${pitch.id}'`, function(err, result) {
+            database.query(`SELECT * FROM pitches WHERE id = '${pitch.id}' AND game = '${game.id}'`, function(err, result) {
               if (err) return c(`Missing pitch: ${pitch.id}`);
               if (result.rows.length == 0) return c(`No pitch found for ${pitch.id}`);
-              if (result.rows.length > 1) return c(`Duplicate rows for pitch ${pitch.id}`);
               var p = result.rows.pop();
               if (p.pfx_x != pitch.pfx_x) {
-                console.log(`Expected pfx_x of ${pitch.pfx_x}, found ${p.pfx_x}`);
+                console.log(`    Expected pfx_x of ${pitch.pfx_x}, found ${p.pfx_x}`);
                 return c(`Data didn't match on pitch ${pitch.id}`);
               }
               if (p.end_speed != pitch.end_speed) {
-                console.log(`Expected end_speed of ${pitch.end_speed}, found ${p.end_speed}`);
+                console.log(`    Expected end_speed of ${pitch.end_speed}, found ${p.end_speed}`);
                 return c(`Data didn't match on pitch ${pitch.id}`);
               }
               c();
             })
           }, function(err) {
             if (err) {
-              console.log(err);
+              console.log(chalk.red(err));
             } else {
-              console.log("Everything checked out!");
+              console.log(chalk.green("Everything checked out!"));
             }
             callback(err);
           });
@@ -69,7 +73,7 @@ for (var i = 0; i < 5; i++) {
 }
 
 async.eachSeries(tests, function(t, c) {
-  console.log(t.title);
+  console.log(chalk.cyan(t.title));
   results.total++;
   t.test(function(err) {
     if (err) {
@@ -80,6 +84,6 @@ async.eachSeries(tests, function(t, c) {
     c();
   });
 }, function() {
-  console.log(`Final result: ${results.passed} passed, ${results.failed} failed, ${results.total} total`);
+  console.log(chalk.bgWhite.black(`Final result: ${results.passed} passed, ${results.failed} failed, ${results.total} total`));
   database.end();
 });
